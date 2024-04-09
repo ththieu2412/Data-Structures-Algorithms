a17010add26a0e823d7ef9b452edf577d43ea326
TrungHieu <ththieu2412@gmail.com>
2024-04-09 21:39:32 +0700

    Add Chuong2/bai1

---
+++ Chuong2/bai1.py
@@ -0,0 +1,20 @@
+def isDoiXung(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return False
+    return True
+
+n = int(input("Nhap ma tran vuong n = : "))
+arr = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        print(f"array[{i}][{j}]: ",end="")
+        row.append(int(input()))
+    arr.append(row)
+
+for i in range(len(arr)):
+    print(arr[i])
+print(isDoiXung(arr))  
+
