540f19253bfc261b6a1eb353ae65162c11680437
TrungHieu <ththieu2412@gmail.com>
2024-04-10 09:27:48 +0700

    Modify bai3.py

--- Chuong2/bai3.py
+++ Chuong2/bai3.py
@@ -2,14 +2,14 @@ import numpy as np
 
 def isTrungHang(arr):
     tuple_arr = [tuple(row) for row in arr]
+
+    #set khong nhan gia tri trung lap
     return len(tuple_arr) != len(set(tuple_arr))
 
 
 def main():
     l1 = [[1,2,3],[5,6,2],[2,6,5]]
-    l2 = [[1,2,3],[0,5,6],[0,0,9]]
     arr1 = np.array(l1)
-    arr2 = np.array(l2)
     print(isTrungHang(arr1))
 
 if __name__ == "__main__":
